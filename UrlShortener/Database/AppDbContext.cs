59704763319025a4f0313ca3b9d361ac0a06bb79
modestsp <csperichon@gmail.com>
2023-07-29 22:47:04 -0300

    Added Urls Model

--- UrlShortener/Database/AppDbContext.cs
+++ UrlShortener/Database/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace UrlShortener.Database;
 
 public class AppDbContext : IdentityDbContext<User>
 {
+    public virtual DbSet<Url> Urls { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
 

83fdc8505ec7a9ada88fc6fd8a1c61450dbd04ed
modestsp <csperichon@gmail.com>
2023-08-01 21:32:40 -0300

    Created Url Model

---
+++ UrlShortener/Models/Url.cs
@@ -0,0 +1,10 @@
+namespace UrlShortener.Models;
+
+public class Url
+{
+    public int Id { get; set; }
+    public string OriginalUrl { get; set; } = "";
+    public string ShortUrl { get; set; } = "";
+    public User? User { get; set; }
+    public string? UserId { get; set; }
+}

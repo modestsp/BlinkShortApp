53a4e42b2428bb280eed870ab4471178ce4632af
modestsp <csperichon@gmail.com>
2023-08-02 23:32:52 -0300

    Updated Migration

---
+++ UrlShortener/Migrations/20230802161127_Added CreatedAt to Url.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCreatedAttoUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Urls",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Urls");
+        }
+    }
+}

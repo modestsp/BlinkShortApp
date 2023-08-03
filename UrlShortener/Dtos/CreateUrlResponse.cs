8be001206bd10a7049cd385159f1beb0d2d1c44b
modestsp <csperichon@gmail.com>
2023-08-02 23:31:33 -0300

    Added Id and CreatedAt props

--- UrlShortener/Dtos/CreateUrlResponse.cs
+++ UrlShortener/Dtos/CreateUrlResponse.cs
@@ -2,5 +2,7 @@ namespace UrlShortener.Dtos;
 
 public class CreateUrlResponse
 {
+    public int Id { get; set; }
     public string ShortUrl { get; set; } = "";
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
